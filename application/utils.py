d626493d400041df3b569bf8d4bc994848194f9c
Adam Shimali <adam.shimali@gmail.com>
2026-02-16 08:39:50 +0000

    fix: file missing from last commit

--- application/utils.py
+++ application/utils.py
@@ -8,9 +8,3 @@ def load_data(filename, encoding="utf-8"):
     path = os.path.join(current_app.config["PROJECT_ROOT"], "data", filename)
     with open(path, encoding=encoding) as f:
         return list(csv.DictReader(f))
-
-
-def timestamp_ms(date_str, fmt="%Y-%m-%d"):
-    from datetime import datetime
-
-    return int(datetime.strptime(date_str.strip(), fmt).timestamp() * 1000)
